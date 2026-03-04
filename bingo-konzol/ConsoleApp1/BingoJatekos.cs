1bb878e4b963e4e1bf76c5b2a630aa8c8baed5c2
BraunRoland <rolibraun94@gmail.com>
2026-03-04 09:27:43 +0100

    FajlBeolvasas()

---
+++ bingo-konzol/ConsoleApp1/BingoJatekos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class BingoJatekos
+	{
+
+		private string nev;
+		private int[,] tabla;
+
+		public BingoJatekos(string nev, int[,] tabla)
+		{
+			this.nev = nev;
+			this.tabla = tabla;
+		}
+
+		override public string ToString()
+		{
+			string str = nev + "\n";
+			for (int i = 0; i < tabla.GetLength(0); i++)
+			{
+				for (int j = 0; j < tabla.GetLength(1); j++)
+				{
+					str += tabla[i, j] + " ";
+				}
+				str += "\n";
+			}
+			return str;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int[,] Tabla { get => tabla; set => tabla = value; }
+	}
+}
--- bingo-konzol/ConsoleApp1/Program.cs
+++ bingo-konzol/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,51 @@ namespace ConsoleApp1
 {
 	internal class Program
 	{
+
+		static void FajlBeolvasas(string fajlnev)
+		{
+			Console.WriteLine($"{fajlnev}:");
+			var sr = new StreamReader($"programozás/{fajlnev}");
+			int x = 0;
+			string[,] tabla = new string[5, 5];
+			while (!sr.EndOfStream)
+			{
+				var sor = sr.ReadLine();
+				var adatok = sor.Split(';');
+				for (int i = 0; i < adatok.Length; i++)
+				{
+					tabla[x, i] = adatok[i];
+				}
+				x++;
+			}
+			Console.WriteLine($"{fajlnev} beolvasása kész.");
+			Console.WriteLine("------------------------------");
+		}
+
+		static string[] Nevek()
+		{ 
+			var sr = new StreamReader("programozás/nevek.text");
+			var file = new StreamReader("programozás/nevek.text").ReadToEnd();
+			var line = file.Split('\n');	
+			string[] tomb = new string[line.Length];
+			int index = 0;
+			while (!sr.EndOfStream)
+			{
+				var sor = sr.ReadLine();
+				tomb[index] = sor;
+				Console.WriteLine($"{sor}");
+				index++;
+			}
+			return tomb;
+
+		}
 		static void Main(string[] args)
 		{
+			string[] nevek = Nevek();
+			for (int i = 0; i < nevek.Length; i++)
+			{
+				FajlBeolvasas(nevek[i]);
+			}
 		}
 	}
 }
